48a6aac6199d63cf876291ec693519526c67fd7c
Dell-User <Dell-User@DESKTOP-M748B2N>
2023-10-09 11:27:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Data_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaration
+            string name = "Anna";
+            int age = 35;
+            bool isTall = false;
+            double weight = 65.5;
+
+            /*for the samallest least precise decimal use float,
+             * for more precise use decimal e.g money
+             * in most cases double will ddo just fine
+             * */
+
+            Console.WriteLine(name + " is " + age + ", she weighs " + weight);
+            Console.WriteLine(" It is" + isTall + " that she is tall.");
+
+            //freeeze console
+            Console.ReadLine();
+
+        }
+    }
+}
